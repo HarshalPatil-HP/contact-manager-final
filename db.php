e6804b74053bdf532924cd1316c3660f55ab3924
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-08-20 22:40:57 +0530

    Remove unnecessary whitespace in db.php

--- db.php
+++ db.php
@@ -1,11 +1,9 @@
 <?php
-
 $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "contacts_db";
 $conn = mysqli_connect($host, $user, $pass, $db);
-
 if(!$conn){
     die("Database Connection Failed!");
 } 
