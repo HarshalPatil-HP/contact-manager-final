7a716b58178c80922f29253a77b76a884b099e86
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-08-27 23:58:53 +0530

    Add database connection to db.php

--- db.php
+++ db.php
@@ -4,7 +4,9 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "contacts_db";
+
 $conn = mysqli_connect($host, $user, $pass, $db);
+
 if(!$conn){
     die("Database Connection Failed!");
 } 
